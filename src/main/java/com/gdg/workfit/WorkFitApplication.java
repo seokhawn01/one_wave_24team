736a5146c32262cb865123982d9762b9b2bdb6b9
seokhawn01 <skhu202214139@gmail.com>
2026-02-06 22:06:22 +0900

    feat/#1: 초기설정

---
+++ src/main/java/com/gdg/workfit/WorkFitApplication.java
@@ -0,0 +1,13 @@
+package com.gdg.workfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkFitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkFitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gdg/workfit/WorkFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdg.workfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkFitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
